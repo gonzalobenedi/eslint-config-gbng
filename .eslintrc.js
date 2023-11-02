8768ec828a71eb072d51262caabbf1c15aaa3e8d
Gonzalo Benedi <gonzalobenedi_dev@outlook.com>
2023-11-02 19:27:14 +0100

    chore: base config

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:prettier/recommended",
+    "prettier/react",
+  ],
+  plugins: ["react-hooks"],
+  rules: {},
+};
